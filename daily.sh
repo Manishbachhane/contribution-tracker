88fc07ba12b73f616ad411e0f235030d8b643517
Manishbachhane <manishbachhane@gmail.com>
2025-07-11 21:49:33 +0530

    Daily log update

---
+++ daily.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/OneDrive/Desktop/final/contribution-tracker
+read -p "What did you do today? " log
+echo "$(date '+%Y-%m-%d') - $log" >> log.txt
+git add .
+git commit -m "Daily log update"
+git push
